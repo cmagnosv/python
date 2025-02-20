bc90ad316e17b3101890b8ae661f084c9575cc3e
Carlos Magno Sousa Vieira <c.magnosv@gmail.com>
2025-02-20 17:24:46 -0300

    nova versão com erro mg_aula_quinta01.py

--- mg_aula_quinta01.py
+++ mg_aula_quinta01.py
@@ -34,15 +34,28 @@ class AnalisadorDeVendas:
         self.dados['dia_da_semana'] = self.dados['data'].dt.weekday # separa o dia da semana sendo 0 a segunda e 6 domingo
         self.dados.dropna(subset=['produto', 'valor'], inplace=True) # remove os dados ausente em clunas importantes
   
-    def analise_vendas_por_produto(self, produtos_filtrados):
-        df_produto = self.dados[self.dados['produto'].isin(produtos_filtrados)]
-        df_produto = df_produto.groupby('produto')['valor'].reset_index().sort_values(by='valor', ascending=True)
+    def analise_vendas_por_produto(self, produtosFiltrados):
+        df_produto = self.dados[self.dados['produto'].isin(produtosFiltrados)]
+        df_produto = df_produto.groupby(['produto'])['valor'].sum().reset_index().sort_values(by='valor', ascending=True)
         fig = px.bar(
             df_produto,
             x='produto',
             y='valor',
             title="Vendas por produto",
             color= "valor"
+        )
+        return fig
+    
+ #grafico de pizza para vendas por região
+    def analise_vendas_por_regiao(self, regioes_filtradas):
+        df_regiao = self.dados[self.dados['regiao'].isin(regioes_filtradas)]
+        df_regiao = df_regiao.groupby('regiao')['valor'].sum().reset_index().sort_values(by='valor', ascending=False)
+        fig = px.pie(
+            df_regiao,
+            names = 'regiao',
+            value = 'valor',
+            title = 'Vendas por Região',
+            color = 'valor'
         )
         return fig
     
@@ -78,32 +91,41 @@ app.layout = html.Div([
         ),
        html.Label('Selecione um Periodo'),
         dcc.DatePickerRange(
-            id = 'date-picker-range',
+            id = 'data-picker-range',
             start_date = df['data'].min().date(),
             end_date = df['data'].max().date(),
-            display_format = 'YYYY-MM-DD',
+            display_format = 'DD/MM/YY',
             style = {'width':'48%'}
         ),
     ], style={'padding':'20px'}),
     # graficos
     html.Div([
-        dcc.Graph(id='grafico-produto')
+        dcc.Graph(id='grafico-produto'),
+        dcc.Graph(id='grafico-regiao')
+        
     ])
 ])
 #------------------------Callbacks
 @app.callback(
     Output('grafico-produto','figure'),
+    Output('grafico-regiao', 'figure'),
     Input('produto-dropdown','value'),
     Input('regiao-dropdown','value'),
-    Input('date-picker-range','start_date'),
-    Input('date-picker-range','end_date')
+    Input('ano-dropdown','value'),
+    Input('data-picker-range','start_date'),
+    Input('data-picker-range','end_date')
+    
 )
+
 
 def upgrade_graphs(produtos, regioes, ano, start_date, end_date):
     try:
         start_date = pd.to_datetime(start_date)
         end_date = pd.to_datetime(end_date)
         fig_produto = analise.analise_vendas_por_produto(produtos)
+        fig_regiao =  analise.analise_vendas_por_regiao(regioes)
+        
+        return fig_produto, fig_regiao  # Deve retornar um único gráfico
         
     except Exception as e:
         print(f'Erro ao atualizar os graficos: {str(e)}')
